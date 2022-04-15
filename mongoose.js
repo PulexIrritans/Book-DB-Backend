9ce2f3f9999ddff80760545bce914a4d669bda85
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-15 11:01:51 +0200

    add save new book

--- mongoose.js
+++ mongoose.js
@@ -20,11 +20,28 @@ mongoose.connect("mongodb://localhost:27017/booksexercise").then(() => {
 //     console.log(data);
 //     process.exit();
 //   });
-// });
+
 
 // get a book by id
 Book.findById("62592c3f1cdb4f29ded3d675").then((data) => {
     console.log(data);
     process.exit()
 });
+
+// create a book and save in the database
+const newBook = Book({
+  title: "Rebellinnen",
+  isbn: "3-88619-230-X",
+  author: "Gillian G. Gaar",
+  description: "Die Geschichte der Frauen in der Rockmusik",
+  published_date: "1992-01-01T23:00:00:000Z",
+  number_of_pages: 462,
+  publisher: "Argument",
+})
+
+newBook.save().then((data) => {
+    console.log(data);
+    process.exit();
+})
+
 });
