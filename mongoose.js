89a1ec7e916faebd56dd9c4bee5eedae53850200
Antje Exner <antjeexner@Antjes-Air.fritz.box>
2022-04-15 10:48:09 +0200

    add get a book by id

--- mongoose.js
+++ mongoose.js
@@ -14,8 +14,17 @@ const Book = mongoose.model("book", bookSchema);
 
 mongoose.connect("mongodb://localhost:27017/booksexercise").then(() => {
   console.log("connected");
-  Book.find({}).then((data) => {
+
+// get all books back  
+//   Book.find({}).then((data) => {
+//     console.log(data);
+//     process.exit();
+//   });
+// });
+
+// get a book by id
+Book.findById("62592c3f1cdb4f29ded3d675").then((data) => {
     console.log(data);
-    process.exit();
-  });
+    process.exit()
+});
 });
